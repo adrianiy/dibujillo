95d6e0ee0bbcc0fe4c0a7898b0149eb8464f5251
adrianiy <adrianiy@ext.inditex.com>
2020-08-27 19:33:06 +0200

    feat(core): add match configurator

---
+++ src/components/configurator/Configurator.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { ColumnLayout, RowLayout } from '../../global/Layout';
+
+import './Configurator.css';
+import Button from '../button/Button';
+import actions from '../../global/utils/store/actions';
+
+export default function Configurator({ privateRoom }) {
+    const [roomId, setRoomId] = useState(null);
+    const user = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const { href } = window.location;
+        const userId = user.name;
+        const time = (new Date()).getDate();
+        setRoomId(`${href}?roomId=${btoa(`${userId}${time}`)}`);
+    }, [user.name]);
+
+    const _copyLink = () => {
+        navigator.clipboard.writeText(roomId);
+        dispatch(actions.toaster.setOkToaster({ text: 'link copiado!' }));
+    };
+
+    const _renderLinkShare = () => (
+        <ColumnLayout className="linkColumn">
+            <h3>Comparte el link con tus amigos!</h3>
+            <Button className="text" icon="link" color="blue" onClick={_copyLink}>
+                Copiar enlace
+            </Button>
+        </ColumnLayout>
+    );
+
+    const _renderFriendsShare = () => (
+        <ColumnLayout className="friendsColumn">
+            <h3>Invita a amigos a tu sala privada!</h3>
+            <input type="text" disabled />
+        </ColumnLayout>
+    );
+
+    const _renderShareSection = () => (privateRoom ? (
+        <ColumnLayout>
+            {_renderLinkShare()}
+            {_renderFriendsShare()}
+        </ColumnLayout>
+    ) : null);
+
+    const _renderBottomButtons = () => (
+        <RowLayout dist="right" className="bottomButtons">
+            <Button className="text" color="blue" icon="settings">
+                Configuración avanzada
+            </Button>
+            <Button icon="play_arrow" className="playButton" color="black">
+                Iniciar
+            </Button>
+        </RowLayout>
+    );
+
+    return (
+        <ColumnLayout className="configuratorContainer">
+            <h2>
+                Configura tu partida
+                { privateRoom ? 'privada' : 'pública' }
+            </h2>
+            { _renderShareSection() }
+            { _renderBottomButtons() }
+        </ColumnLayout>
+    );
+}
+
+Configurator.propTypes = {
+    privateRoom: PropTypes.bool.isRequired,
+};
--- src/pages/landing/components/LandingForms.js
+++ src/pages/landing/components/LandingForms.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { RowLayout, ColumnLayout } from '../../../global/Layout';
-import { Button } from '../../../components/button/Button';
-import { cls } from '../../../global/utils';
-
 import GoogleLogin from 'react-google-login';
+import { RowLayout, ColumnLayout } from '../../../global/Layout';
+import Button from '../../../components/button/Button';
+import Configurator from '../../../components/configurator/Configurator';
+import cls from '../../../global/utils';
+import actions from '../../../global/utils/store/actions';
 
 import styles from '../Landing.module.css';
-import actions from '../../../global/utils/store/actions';
 
 export default function LandingForms() {
     const [roomId, setRoomId] = useState('');
-    const [user] = useSelector(state => [state.user]);
+    const [showConfigurator, setShorwConfigurator] = useState(0);
+    const user = useSelector((state) => state.user);
     const dispatch = useDispatch();
 
     /* istanbul ignore function */
@@ -19,66 +20,73 @@ export default function LandingForms() {
         if (response.profileObj) {
             dispatch(actions.user.setUser(response.profileObj));
         }
-    }
+    };
 
-    const _renderJoinForm = () => {
-        return (
-            <form>
-                <input
-                    className={styles.input}
-                    data-testid="room-id"
-                    type="text"
-                    placeholder="#IdLienzo"
-                    value={roomId}
-                    onChange={(e) => setRoomId(e.target.value)}
-                />
-            </form>
-        )
-    }
-
-    const _renderRoomSelectorInputs = () => {
-        return (
-            <RowLayout dist="row middle spaced" className={cls(styles.container, 'animated fadeIn')}>
-                <ColumnLayout dist="spaced center" className={styles.inputContainer}>
-                    <h3>Crea un nuevo lienzo!</h3>
-                    <Button icon="lock" color="black">
-                        Crea una partida privada
-                    </Button>
-                    <Button icon="public" color="black">
-                        Crea una partida publica
-                    </Button>
-                </ColumnLayout>
-                <ColumnLayout dist="spaced center" className={styles.inputContainer}>
-                    <h3>Unete a un lienzo ya creado!</h3>
-                    { _renderJoinForm() }
-                    <Button icon="search">
-                        Busca una partida publica
-                    </Button>
-                </ColumnLayout>
-            </RowLayout>
-        )
-    }
-
-    const _renderLoginInputs = () => {
-        return (
-            <GoogleLogin
-                clientId={process.env.REACT_APP_GOOGLE_KEY}
-                buttonText="Login con Google"
-                onSuccess={_googleResponse}
-                onFailure={_googleResponse}
-                isSignedIn={true}
-                cookiePolicy={'single_host_origin'}
+    const _renderJoinForm = () => (
+        <form>
+            <input
+                className={styles.input}
+                data-testid="room-id"
+                type="text"
+                placeholder="#IdLienzo"
+                value={roomId}
+                onChange={(e) => setRoomId(e.target.value)}
             />
-        )
-    }
+        </form>
+    );
 
+    const _showConfigurator = (privateRoom = false) => {
+        if (privateRoom) {
+            setShorwConfigurator(1);
+        } else {
+            setShorwConfigurator(2);
+        }
+    };
+
+    const _renderRoomSelectorInputs = () => (
+        <RowLayout dist="row middle spaced" className={cls(styles.container, 'animated fadeIn')}>
+            <ColumnLayout dist="spaced center" className={styles.inputContainer}>
+                <h3>Crea un nuevo lienzo!</h3>
+                <Button icon="lock" color="black" onClick={() => _showConfigurator(true)}>
+                    Crea una partida privada
+                </Button>
+                <Button icon="public" color="black" onClick={() => _showConfigurator(false)}>
+                    Crea una partida publica
+                </Button>
+            </ColumnLayout>
+            <ColumnLayout dist="spaced center" className={styles.inputContainer}>
+                <h3>Unete a un lienzo ya creado!</h3>
+                { _renderJoinForm() }
+                <Button icon="search">
+                    Busca una partida publica
+                </Button>
+            </ColumnLayout>
+        </RowLayout>
+    );
+
+    const _renderLoginInputs = () => (
+        <GoogleLogin
+            clientId={process.env.REACT_APP_GOOGLE_KEY}
+            buttonText="Login con Google"
+            onSuccess={_googleResponse}
+            onFailure={_googleResponse}
+            isSignedIn
+            cookiePolicy="single_host_origin"
+        />
+    );
+
+    const _renderConfigurator = () => (
+        <Configurator privateRoom={showConfigurator === 1} />
+    );
 
     return (
         <RowLayout dist="middle center" className={styles.inputsWrapper}>
             {
                 user.loggedIn ? _renderRoomSelectorInputs() : _renderLoginInputs()
             }
+            {
+                showConfigurator > 0 ? _renderConfigurator() : null
+            }
         </RowLayout>
-    )
-
+    );
 }
